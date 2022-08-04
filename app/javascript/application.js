db6bdf735d17f0debc131456d39af93638a3c28b
Steven Banks <stevenb@crosslaketech.com>
2022-08-04 19:46:01 -0400

    have working React hello world app integrated with rails and served by esbuild

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,10 @@
-// Entry point for the build script in your package.json
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import HelloMessage from './components/App';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+document.addEventListener('DOMContentLoaded', () => {
+  root.render(<HelloMessage name="Jimbo" />);
+});
