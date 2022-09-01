04e5f35f13078cda41428796c8e1c3c0c568b9db
Steven Banks <stevenb@crosslaketech.com>
2022-09-01 19:37:29 -0400

    handle entire front-end workflow of submitting name, fetching chores, and displaying a warning if user not found with ability to start over

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,6 +2,6 @@ import React from 'react';
 import DailyChoreList from './DailyChoreList';
 import './App.css';
 
-const App = () => <DailyChoreList name="Barry Allen" />;
+const App = () => < DailyChoreList />;
 
 export default App;
--- app/javascript/components/DailyChoreList.js
+++ app/javascript/components/DailyChoreList.js
@@ -1,28 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { getDailyChoreCalendarForUser } from '../services/ChoreCalendarService';
+import { isEmpty } from 'lodash';
 
-const DailyChoreList = ({ name }) => {
-  const [chores, setChores] = useState([]);
-
-  useEffect(() => {
-    getDailyChoreCalendarForUser("Joshua", "2022-08-12")
-      .then(response => {
-        setChores(response);
-      });
-  }, []);
+const DailyChoreList = () => {
+  const [chores, setChores] = useState(null);
+  const [name, setName] = useState("");
+  const [isFetched, setIsFetched] = useState(false);
 
   const handleChoreCheckChanged = (index) => {
     newChores = [...chores];
-    console.log(`hello mate! ${index}, checked = ${newChores[index].checked}`);
     newChores[index].checked = !newChores[index].checked;
     setChores(newChores);
   };
 
+  const startOver = () => {
+    setIsFetched(false);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getDailyChoreCalendarForUser(name, "2022-08-12")
+      .then(response => {
+        setIsFetched(true);
+        setChores(response);
+      });
+  };
+
   return (
     <>
-      <h1>Hello, {name}!</h1>
+      {!isFetched && <form onSubmit={handleSubmit}>
+        <label>
+          Please enter your name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>}
 
-      <div className="grid">
+      {isFetched && isEmpty(chores) &&
+        <>
+          <h1>Sorry {name}, we could not find you in our system.</h1>
+          <button onClick={() => startOver()}>Start Over</button>
+        </>
+      }
+      {!isEmpty(chores) && <h1>Hello, {name}! Here are your chores for today:</h1>}
+      {!isEmpty(chores) && <div className="grid">
         {chores.map((chore, index) => {
           return (
             <>
@@ -37,7 +62,7 @@ const DailyChoreList = ({ name }) => {
             </>
           );
         })}
-      </div>
+      </div>}
     </>
   );
 };
