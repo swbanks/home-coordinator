3e2782f02b35c9108e71dd1df741c45ad15965b5
Steven Banks <stevenb@crosslaketech.com>
2022-12-25 17:19:50 -0500

    add dependency and helper comments

--- app/services/chore_calendar_service.rb
+++ app/services/chore_calendar_service.rb
@@ -1,5 +1,6 @@
 module ChoreCalendarService
   def self.get_daily_chores_for_user(user_name, date)
+    # when to schedule daily chores. Check yesterday to see if the daily chores were completed. If not
     user = User.find_by(name: user_name)
     ChoreCalendar.includes(:chore).where(user_id: user&.id, chore_date: date)
   end
@@ -8,6 +9,8 @@ module ChoreCalendarService
     chore_list.each do |chore|
       chore_calendar = ChoreCalendar.find(chore[:id])
       chore_calendar.update!(user_completed: chore[:user_completed], checked: chore[:checked])
+
+      # can I find the chore_config record? If so and its a daily, schedule tomorrow for the next user
     end
   end
 end
